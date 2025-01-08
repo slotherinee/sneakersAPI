f85984643c7fc38b76f97d9c15c878b332acc2a8
Pavel Tarasov <testfordev121@gmail.com>
2025-01-09 00:29:13 +0300

    register routes

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import express, { Express } from 'express';
 import userRouter from '@routes/user.routes';
+import sneakerRouter from '@routes/sneaker.routes';
+import orderRouter from '@routes/order.routes';
 
 const app: Express = express();
 app.use(express.json());
 
 app.use('/api/users', userRouter)
+app.use('/api/sneakers', sneakerRouter)
+app.use('/api/orders', orderRouter)
 
 export default app;
